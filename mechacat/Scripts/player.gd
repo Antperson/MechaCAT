25ef9b37a99db39f0bacea2d2d895f61399dfcf6
Starrite <jimmythai19@gmail.com>
2025-03-16 15:24:23 -0400

    Tried to add bullet

--- mechacat/Scripts/player.gd
+++ mechacat/Scripts/player.gd
@@ -4,6 +4,8 @@ const SPEED = 300.0
 var isSlashing = false
 var isShooting = false
 
+const Bullet = preload("res://Scenes/bullet.tscn")
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * SPEED
@@ -13,18 +15,22 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func _process(delta):
-	if (velocity == Vector2.ZERO):
+	if (velocity != Vector2.ZERO):
 		$AnimatedSprite2D.play("Idle")
-	elif Input.is_action_just_pressed("slash") && isShooting == false:
-		isSlashing == true
+			
+	if Input.is_action_just_pressed("slash") && isShooting == false:
 		$AnimatedSprite2D.play("Slash")
-	elif Input.is_action_just_pressed("shoot")&& isSlashing == false:
-		isShooting == true
+		isSlashing == true
+		
+	if Input.is_action_just_pressed("shoot")&& isSlashing == false:
+		var b = Bullet.instance()
+		owner.add_child(b)
+		b.transform = $Gun.global_transform
 		$AnimatedSprite2D.play("Shoot")
+		isShooting == true
 		
 func on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite2D.animation == "Slash":
 		isSlashing == false
 	if $AnimatedSprite2D.animation == "Shoot":
 		isShooting == false
-	
