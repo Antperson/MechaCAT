c7f36944ef503abe24e0edc899fd86e895c99958
Antperson <altantonio1125@gmail.com>
2025-03-20 15:28:08 -0400

    Bullet despawn edit
    They now clear when they go off screen. Next we just need to tack on hp code and we are good for handling combat.

--- mechacat/Scripts/bullet.gd
+++ mechacat/Scripts/bullet.gd
@@ -11,3 +11,7 @@ func _on_Bullet_body_entered(body):
 func _on_VisibilityNotifier2D_screen_exited():
 	# free Bullet
 	queue_free()
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
+	queue_free()
--- mechacat/Scripts/enemy_bullet.gd
+++ mechacat/Scripts/enemy_bullet.gd
@@ -4,21 +4,18 @@ extends Node2D
 @export var offset: float = 0.0
 @export var frequency: int = 0
 @export var amplitude: float = 0.0
-@onready var timer: Timer = $Area2D/Timer
 
 func _process(delta: float) -> void:
-	timer.start()
 	offset += delta
 	position += transform.x * speed * delta + transform.y * sin(offset * frequency) * amplitude
 
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("true")
 	queue_free()
+	
 	
 
 
-func _on_timer_timeout() -> void:
-	print("false")
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
