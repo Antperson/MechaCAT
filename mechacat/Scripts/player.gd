ae26e553b4dece47d90e1658ea698647d5617b46
Starrite <jimmythai19@gmail.com>
2025-03-26 17:31:10 -0400

    Animations

--- mechacat/Scripts/player.gd
+++ mechacat/Scripts/player.gd
@@ -18,22 +18,13 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func _process(delta):
-	"""if (velocity == Vector2.ZERO):
-		$AnimatedSprite2D.play("Idle")
-	elif Input.is_action_just_pressed("slash") && isShooting == false:
+	if Input.is_action_just_pressed("slash") && isShooting == false:
 		isSlashing == true
 		$AnimatedSprite2D.play("Slash")
-	el"""
 	if Input.is_action_just_pressed("shoot")&& isSlashing == false:
 		shoot()
 		isShooting == true
 		$AnimatedSprite2D.play("Shoot")
-		
-func on_AnimatedSprite_animation_finished():
-	if $AnimatedSprite2D.animation == "Slash":
-		isSlashing == false
-	if $AnimatedSprite2D.animation == "Shoot":
-		isShooting == false
 		
 func shoot():
 	var b = bullet.instantiate()
@@ -62,3 +53,12 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	e.position = global_position
 	e.rotation = global_rotation
 	get_parent().add_child(e)
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation == "Slash":
+		isSlashing == false
+		$AnimatedSprite2D.play("Idle")
+	if $AnimatedSprite2D.animation == "Shoot":
+		isShooting == false
+		$AnimatedSprite2D.play("Idle")
